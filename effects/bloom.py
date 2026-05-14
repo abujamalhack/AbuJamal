ac10af2e6c3cc9e16e5a1212d0aa55fa404ebdd0
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2026-05-14 19:11:58 +0300

    Create bloom.py

---
+++ effects/bloom.py
@@ -0,0 +1,8 @@
+class BloomEffect:
+    def __init__(self, intensity=0.8):
+        self.enabled = True
+        self.intensity = intensity
+
+    async def apply(self, target):
+        # محاكاة تأثير البلوم عبر تغيير الألوان مؤقتاً
+        pass
