20c5ef2709493791c18809eaf6aefdcf0485857a
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2026-05-14 18:37:29 +0300

    Add themes configuration for UI customization

---
+++ themes/themes.py
@@ -0,0 +1,5 @@
+THEMES = {
+    'crimson': {'background': '#000000', 'primary': '#DC143C', 'secondary': '#8B0000'},
+    'neon': {'background': '#000000', 'primary': '#FF00FF', 'secondary': '#00FFFF'},
+    'dark': {'background': '#0A0A0A', 'primary': '#00FF00', 'secondary': '#00AA00'},
+}
